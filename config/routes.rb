41572a080d2bab62dc2aa3c3cf48e18768419326
Harry Bremner <harrybremner@gmail.com>
2022-05-25 10:43:06 +0100

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,27 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'articles#index'
+
+  resources :articles
 end
+
+
+#   get "tasks", to: "tasks#index"
+#   get "tasks/new", to: "tasks#new"
+#   get "tasks/:id", to: "tasks#show", as: "task"
+#   post "tasks", to: "tasks#create"
+#   get "tasks/:id/edit", to: "tasks#edit"
+#   patch "tasks/:id", to: "tasks#update"
+#   delete "tasks/:id", to: "tasks#destroy"
+
+# index    # Read all resources
+# show     # Read one resource
+
+# new      # display form for a resource creation
+# create   # Create resource
+
+# edit     # display form for a resource update
+# update   # Update resource
+
+# destroy  # Delete resource
